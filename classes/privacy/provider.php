f14ebae1463b75f5c8581350c1a46a041865f9df
angelitro <angelluisfraile@gmail.com>
2019-05-21 18:31:43 +0200

    Update provider.php
    namespaces privacy

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -19,6 +19,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace tool_globaladtool\privacy;
+
 class provider implements
     // This plugin does not store any personal user data.
     \core_privacy\local\metadata\null_provider {
